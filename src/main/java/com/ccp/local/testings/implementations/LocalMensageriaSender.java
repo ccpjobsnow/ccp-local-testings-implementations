1252706b1ea715854a3e8034d2ece08660222393
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2025-04-12 01:26:15 -0300

    .

--- src/main/java/com/ccp/local/testings/implementations/LocalMensageriaSender.java
+++ src/main/java/com/ccp/local/testings/implementations/LocalMensageriaSender.java
@@ -1,6 +1,9 @@
 package com.ccp.local.testings.implementations;
 
 
+import java.lang.reflect.Field;
+import java.util.function.Function;
+
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.especifications.mensageria.sender.CcpMensageriaSender;
 
@@ -8,6 +11,17 @@ public class LocalMensageriaSender implements CcpMensageriaSender {
 
 	public LocalMensageriaSender() {
 
+	}
+	
+	private Function<CcpJsonRepresentation, CcpJsonRepresentation> getProcess(String processName, CcpJsonRepresentation json){
+		try {
+			Class<?> forName = Class.forName("com.ccp.jn.commons.mensageria.JnMensageriaReceiver");
+			Field declaredField = forName.getDeclaredField("INSTANCE");
+			Object object = declaredField.get(null);
+			return null;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public CcpMensageriaSender send(String topic, String... msgs) {
