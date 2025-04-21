468883ece95862afd1b3505da3ed075581180be4
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2025-04-21 18:33:28 -0300

    .

--- src/main/java/com/ccp/local/testings/implementations/LocalMensageriaSender.java
+++ src/main/java/com/ccp/local/testings/implementations/LocalMensageriaSender.java
@@ -1,10 +1,9 @@
 package com.ccp.local.testings.implementations;
 
 
-import java.lang.reflect.Field;
-import java.util.function.Function;
-
 import com.ccp.decorators.CcpJsonRepresentation;
+import com.ccp.especifications.mensageria.receiver.CcpMensageriaReceiver;
+import com.ccp.especifications.mensageria.receiver.CcpTopic;
 import com.ccp.especifications.mensageria.sender.CcpMensageriaSender;
 
 public class LocalMensageriaSender implements CcpMensageriaSender {
@@ -12,29 +11,19 @@ public class LocalMensageriaSender implements CcpMensageriaSender {
 	public LocalMensageriaSender() {
 
 	}
-	
-	private Function<CcpJsonRepresentation, CcpJsonRepresentation> getProcess(String processName, CcpJsonRepresentation json){
-		try {
-			Class<?> forName = Class.forName("com.ccp.jn.commons.mensageria.JnMensageriaReceiver");
-			Field declaredField = forName.getDeclaredField("INSTANCE");
-			Object object = declaredField.get(null);
-			return null;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
 
 	public CcpMensageriaSender send(String topic, String... msgs) {
 
 		for (String msg : msgs) {
-			//FIXME
-			CcpJsonRepresentation messageDetails = new CcpJsonRepresentation(msg);
+			CcpJsonRepresentation json = new CcpJsonRepresentation(msg);
 //			new Thread(() -> {
 //				Function<CcpJsonRepresentation, CcpJsonRepresentation> process = CcpAsyncTask.getProcess(topic);
 //				process.apply(messageDetails); 
 //			}).start();
 
-			
+			CcpMensageriaReceiver receiver = new CcpMensageriaReceiver(topic, "operation");
+			CcpTopic process = receiver.getProcess(topic, json);
+			process.apply(json);
 			
 		}
 		return this;
